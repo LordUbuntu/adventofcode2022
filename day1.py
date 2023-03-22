d0185493c405ecfab2199661c862705b52925387
Jacobus Burger <therealjacoburger@gmail.com>
2023-03-22 16:19:22 -0700

    remove verbose doc

--- day1.py
+++ day1.py
@@ -2,19 +2,6 @@
 
 
 class Solution:
-    def __doc__(self):
-        return """
-        # Day 1
-
-        ## Part 1 and 2
-
-        This entire day was trivial. In both part 1 and 2 we just
-        get the max calories from the list after summing all the
-        calories for each elf. Finally, we return either the greatest
-        number of calories or the sum of the three greatest.
-    """
-
-
     def part1(self, file="day1.txt"):
         # record total calories of each elf inventory
         calories = [
--- day3.py
+++ day3.py
@@ -4,36 +4,6 @@ from string import ascii_letters
 
 
 class Solution:
-    def __doc__(self):
-        return """
-        # Day 3
-
-        ## Part 1
-
-        ### Explanation
-
-        Given a sack (string), I need to split it into two
-        compartments (strings) and determine what item (character)
-        they have in common, then get the priority value of that
-        item, and add that together to get the total priority of all.
-
-        To do this I just wrote a function to calculate the correct
-        priority levels (a-z == 1-26, A-Z == 27-52). Then, iterating
-        through every string, I use python's set intersection to find
-        the common item, use the priority function to calculate the
-        value, and then add it to a running total then return that total.
-
-        ## Part 2
-
-        ### Explanation
-
-        To solve this, I take groups of 3 sacks at a time and
-        find the intersection between all three of them with
-        a reduce. Then add the priority value of the answer to the
-        running total and return that running total.
-    """
-
-
     def parse(self, filename="day3.txt"):
         with open(filename, 'r') as file:
             return [
